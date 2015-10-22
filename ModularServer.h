a6d4fd42f6f7ab81d56760899e22bd446f573270
debris <marek.kotewicz@gmail.com>
2015-10-22 11:44:55 +0800

    modular server is working

--- ModularServer.h
+++ ModularServer.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <jsonrpccpp/common/procedure.h>
 #include <jsonrpccpp/server/iprocedureinvokationhandler.h>
+#include <jsonrpccpp/server/abstractserverconnector.h>
+#include <jsonrpccpp/server/requesthandlerfactory.h>
 
 template <class I> using AbstractMethodPointer = void(I::*)(Json::Value const& _parameter, Json::Value& _result);
 template <class I> using AbstractNotificationPointer = void(I::*)(Json::Value const& _parameter);
@@ -54,23 +56,44 @@ template <class... Is>
 class ModularServer: public jsonrpc::IProcedureInvokationHandler
 {
 public:
+	ModularServer(jsonrpc::AbstractServerConnector* _connector)
+	: m_connection(_connector), m_handler(jsonrpc::RequestHandlerFactory::createProtocolHandler(jsonrpc::JSONRPC_SERVER_V2, *this))
+	{
+		m_connection->SetHandler(m_handler.get());
+	}
+	
+	bool StartListening() { return m_connection->StartListening(); }
+	bool StopListening() { return m_connection->StopListening(); }
+	
 	virtual void HandleMethodCall(jsonrpc::Procedure& _proc, const Json::Value& _input, Json::Value& _output) override {}
 	virtual void HandleNotificationCall(jsonrpc::Procedure& _proc, const Json::Value& _input) override {}
+	
+private:
+	std::unique_ptr<jsonrpc::AbstractServerConnector> m_connection;
+protected:
+	std::unique_ptr<jsonrpc::IProtocolHandler> m_handler;
 };
 
-template <class I, class... Is> class ModularServer<I, Is...> : ModularServer<Is...>
+template <class I, class... Is>
+class ModularServer<I, Is...> : public ModularServer<Is...>
 {
 public:
 	using MethodPointer = AbstractMethodPointer<I>;
 	using NotificationPointer = AbstractNotificationPointer<I>;
 	
-	ModularServer<I, Is...>(I* _i, Is*... _is): ModularServer<Is...>(_is...), m_interface(_i)
+	ModularServer<I, Is...>(jsonrpc::AbstractServerConnector* _connector, I* _i, Is*... _is): ModularServer<Is...>(_connector, _is...), m_interface(_i)
 	{
 		for (auto const& method: m_interface->methods())
+		{
 			m_methods[method.procedure().GetProcedureName()] = method.call();
+			this->m_handler->AddProcedure(method.procedure());
+		}
 		
 		for (auto const& notification: m_interface->notifications())
+		{
 			m_notifications[notification.procedure().GetProcedureName()] = notification.call();
+			this->m_handler->AddProcedure(notification.procedure());
+		}
 	}
 	
 	virtual void HandleMethodCall(jsonrpc::Procedure& _proc, const Json::Value& _input, Json::Value& _output) override
--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+#include <jsonrpccpp/server/connectors/httpserver.h>
 #include "ModularServer.h"
 #include "AbstractWebThreeStubServerModule.h"
 
@@ -22,9 +24,6 @@ private:
 	std::string m_method;
 };
 
-///
-
-
 class AbstractEth: public ServerInterface<AbstractEth> {
 public:
 	AbstractEth()
@@ -46,21 +45,20 @@ public:
 	}
 };
 
-
 int main(int argc, char** argv) {
 	
-	ModularServer<SingleMethodServer, SingleMethodServer, Eth> ms(
+	ModularServer<SingleMethodServer, SingleMethodServer, Eth> ms(new jsonrpc::HttpServer(8080, "", "", 2),
 																  new SingleMethodServer("first"),
 																  new SingleMethodServer("second"),
 																  new Eth);
+	ms.StartListening();
 
 	
+	while (true)
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	
-	jsonrpc::Procedure proc("eth_gasPrice", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_STRING,  NULL);
-	Json::Value input(Json::nullValue);
-	Json::Value output(Json::nullValue);
-	
-	ms.HandleMethodCall(proc, input, output);
+	// curl -X POST --data '{"id":8,"jsonrpc":"2.0","method":"eth_gasPrice","params":[]}' -H "Content-Type: application/json" http://localhost:8080
+	// curl -X POST --data '{"id":8,"jsonrpc":"2.0","method":"first","params":[]}' -H "Content-Type: application/json" http://localhost:8080
 	
 	return 0;
 }
