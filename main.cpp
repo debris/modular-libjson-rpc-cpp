2c3bf2d797c79b287b71eb17c14ae864df6f224c
debris <marek.kotewicz@gmail.com>
2015-10-29 13:34:01 +0800

    removed redundant files

--- main.cpp
+++ main.cpp
@@ -1,63 +1,64 @@
 #include <thread>
 #include <jsonrpccpp/server/connectors/httpserver.h>
-#include "ModularServer.h"
-#include "AbstractWebThreeStubServerModule.h"
+#include "example/AInterfaceModule.h"
+#include "example/BInterfaceModule.h"
+#include "example/CInterfaceModule.h"
 
-/// example
-/// example
-/// example
-/// example
-/// example
-
-class SingleMethodServer: public ServerInterface<SingleMethodServer> {
+class AModule: public AInterface
+{
 public:
-	SingleMethodServer(std::string const& _method): m_method(_method)
+	virtual std::string first(const std::string& param1) override
 	{
-		bindAndAddMethod(jsonrpc::Procedure(m_method, jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_STRING,  NULL), &SingleMethodServer::singleMethod);
+		return "first";
 	}
 	
-	inline virtual void singleMethod(const Json::Value &request, Json::Value &response) {
-		response = "hello from " + m_method;
-	};
-	
-private:
-	std::string m_method;
-};
-
-class AbstractEth: public ServerInterface<AbstractEth> {
-public:
-	AbstractEth()
+	virtual std::string second() override
 	{
-		bindAndAddMethod(jsonrpc::Procedure("eth_gasPrice", jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_STRING,  NULL), &AbstractEth::eth_gasPriceI);
+		return "second";
 	}
-
-	inline virtual void eth_gasPriceI(const Json::Value &request, Json::Value &response) {
-		response = eth_gasPrice();
-	};
-	
-	virtual std::string eth_gasPrice() = 0;
 };
 
-class Eth: public AbstractEth {
+class BModule: public BInterface
+{
 public:
-	virtual std::string eth_gasPrice() override {
-		return "1000";
+	virtual std::string up(const std::string& param1) override
+	{
+		return "up";
+	}
+	
+	virtual std::string down() override
+	{
+		return "down";
+	}
+};
+
+class CModule: public CInterface
+{
+public:
+	virtual std::string hello(const std::string& param1) override
+	{
+		return "hello";
+	}
+	
+	virtual std::string world() override
+	{
+		return "world";
 	}
 };
 
 int main(int argc, char** argv) {
+
+	ModularServer<AInterface, BInterface, CInterface> ms(new AModule(), new BModule(), new CModule());
 	
-	ModularServer<SingleMethodServer, SingleMethodServer, Eth> ms(new SingleMethodServer("first"),
-																  new SingleMethodServer("second"),
-																  new Eth);
 	ms.addConnector(new jsonrpc::HttpServer(8080, "", "", 2));
 	ms.StartListening();
 
 	while (true)
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	
-	// curl -X POST --data '{"id":8,"jsonrpc":"2.0","method":"eth_gasPrice","params":[]}' -H "Content-Type: application/json" http://localhost:8080
-	// curl -X POST --data '{"id":8,"jsonrpc":"2.0","method":"first","params":[]}' -H "Content-Type: application/json" http://localhost:8080
+
+
+//	 curl -X POST --data '{"id":8,"jsonrpc":"2.0","method":"up","params":[""]}' -H "Content-Type: application/json" http://localhost:8080
+//	 curl -X POST --data '{"id":8,"jsonrpc":"2.0","method":"first","params":[""]}' -H "Content-Type: application/json" http://localhost:8080
 	
 	return 0;
 }
